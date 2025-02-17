772ed4cddec3e2efa33baf678e8fd0b744991c25
Joey35233 <111520419+josephclampett-education@users.noreply.github.com>
2025-02-17 18:43:09 +0000

    Restored post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,5 +1,10 @@
 # Install `oc` CLI tool
-wget -O /tmp/oc.tgz "https://github.com/okd-project/okd/releases/download/4.15.0-0.okd-2024-03-10-010116/openshift-client-linux-4.15.0-0.okd-2024-03-10-010116.tar.gz"
+arch="$(arch)"
+case "$arch" in
+    x86_64) export TARGET='' ;;
+    aarch64) export TARGET='arm64-' ;;
+esac
+wget -O /tmp/oc.tgz "https://github.com/okd-project/okd/releases/download/4.15.0-0.okd-2024-03-10-010116/openshift-client-linux-${TARGET}4.15.0-0.okd-2024-03-10-010116.tar.gz"
 pushd /tmp
 tar -xvzf oc.tgz
 sudo mv oc /usr/bin/oc
